d28810dea1361cec8f9993fa1b3ae6b81e81f845
MiguelJuBa <migueljuradobaena@gmail.com>
2018-04-04 23:06:46 +0200

    Added catch testing library.

---
+++ src/tests.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <catch.hpp>
+#include "L.hpp"
+
+int main(int argc, char ** argv)
+{
+    Logger::L L;
+    L.setLevel("TRACE");
+    L.i("Testing log...");
+
+    return 0;
+}
