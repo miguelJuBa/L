baa8f310c19c741202b4ca82e859cae38eaf7519
MiguelJuBa <migueljuradobaena@gmail.com>
2018-04-07 00:42:09 +0200

    Added first real test version

---
+++ tests/tests.cpp
@@ -0,0 +1,18 @@
+#define CATCH_CONFIG_MAIN
+#include <iostream>
+#include "../external/catch.hpp"
+#include "../src/L.hpp"
+
+TEST_CASE("Test standard logger output")
+{
+    Logger::L L;
+    std::ostringstream output;
+    L.setLevel("ERROR");
+    L.setChannel(&output);
+    L.t("Filtered out");
+    REQUIRE(output.str().length() == 0);
+    L.setLevel("TRACE");
+    L.t("Hello world");
+    REQUIRE(output.str().length() > std::string("Hello World").length());
+    REQUIRE(output.str() != "");
+}
